c18f0c4a45b8856ce07c4ad3c3ab89de59f8ae3f
flexusISF <146860748+flexusISF@users.noreply.github.com>
2024-02-22 09:24:40 -0800

    Hazard Script Done and Working with Hazard Sprite

---
+++ Assets/RussellPhase1/Scripts/TestMov.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMov : MonoBehaviour
+{
+    private float horizontalInput;
+    private float verticalInput;
+    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector2.right * horizontalInput * moveSpeed * Time.deltaTime);
+        transform.Translate(Vector2.up * verticalInput * moveSpeed * Time.deltaTime);
+    }
+}
